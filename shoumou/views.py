a2667d273633a66caa254238d910585cb59538a0
michitasato <nezimakidorizenmai@yahoo.co.jp>
2019-04-15 13:57:30 +0900

    add data

--- shoumou/views.py
+++ shoumou/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+def index(request):
+    params = {
+        'title':'Hello/Index',
+        'msg':'これは、サンプルで作ったページです。',
+        'goto':'next',
+        }
+    
+    return render(request, 'index.html', params)
+
+def next(request):
+    params = {
+        'title':'Hello/Next',
+        'msg':'もう一つのページです。',
+        'goto':'index',
+        }
+    
+    return render(request, 'index.html', params)
